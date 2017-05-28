1570085a86be62f7da97b5d4c591c0e31567f841
Lakmal Caldera <lakmal.developer@gmail.com>
2017-05-28 09:42:14 +0530

    Add support for the CHANGE NAME ACTION

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -2,15 +2,34 @@ var redux = require('redux');
 
 console.log('Starting redux example');
 
-var reducer = (state = {name: 'Anonyous'}, action) => {
-  //state = state || {name: 'Anonyous'}
-
-  return state;
+var stateDefault = {
+  name: 'Anonyous'
 };
-var store = redux.createStore(reducer);
+var reducer = (state = stateDefault, action) => {
+  console.log('New Action ', action);
 
+  switch(action.type){
+    case 'CHANGE_NAME':
+      return {
+        ...state,
+        name: action.name
+      }
+    break;
+    default:
+    return state;
+  }
+};
+
+var store = redux.createStore(reducer);
 var currentState = store.getState();
-console.log('currentState ', currentState);
+console.log('currentState of todo App: ', currentState);
+
+store.dispatch({
+  type: 'CHANGE_NAME',
+  name: 'Lakmal'
+});
+
+console.log('Name should be andrew: ', store.getState());
 
 
 
--- app/redux-todo-example.jsx
+++ app/redux-todo-example.jsx
@@ -9,10 +9,27 @@ var stateDefault = {
   todos: []
 };
 var reducer = (state = stateDefault, action) => {
+  console.log('New Action ', action);
 
-  return state;
+  switch(action.type){
+    case 'CHANGE_SEARCH_TEXT':
+      return {
+        ...state,
+        searchText: action.searchText
+      }
+    break;
+    default:
+    return state;
+  }
 };
 
 var store = redux.createStore(reducer);
 var currentState = store.getState();
-console.log('currentState of todo App: ', currentState);
+console.log('Current Todo App State: ', currentState);
+
+store.dispatch({
+  type: 'CHANGE_SEARCH_TEXT',
+  searchText: 'Go to Sleep'
+});
+
+console.log('New Todo App State: ', store.getState());
